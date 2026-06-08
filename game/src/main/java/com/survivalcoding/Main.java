e48b438af30e46a51703f140e867ed5dd1e05101
MaybeLemon <maybelemon@naver.com>
2026-06-08 12:47:11 +0900

    260608_gradle_Main 커밋

---
+++ game/src/main/java/com/survivalcoding/Main.java
@@ -0,0 +1,17 @@
+package com.survivalcoding;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // 인스턴스 생성
+        // Hero hero = new Hero();
+        // int a = 10;
+        // long l = 10L; // literal
+
+        Cleric cleric = new Cleric("클레릭", 50, 10);
+
+        System.out.println(cleric.maxHp);
+        System.out.println(cleric.maxMp);
+    }
+}
