3f4e19398761d49d366e9dd1a2e91017e8df0dcf
MaybeLemon <maybelemon@naver.com>
2026-06-09 14:58:52 +0900

    260609_상수 대문자 및 연습문제2

--- game/src/main/java/com/survivalcoding/Main.java
+++ game/src/main/java/com/survivalcoding/Main.java
@@ -2,9 +2,7 @@ package com.survivalcoding;
 
 public class Main {
     public static void main(String[] args) {
-        Cleric cleric = new Cleric("클레릭", 30, 7);
-
-        int mpRecovery = cleric.pray(3);
-        System.out.println(mpRecovery);
+//        int mpRecovery = cleric.pray(3);
+//        System.out.println(mpRecovery);
     }
 }
